0966af4d1d5582f139fb3eeddb32feabbc882b6f
Katy Molony <85511799+k8molony@users.noreply.github.com>
2023-03-06 11:45:17 -0500

    added vercel.json

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const Users = Models.User;
 
 //mongoose.connect('mongodb://localhost:27017/superFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
 
-mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.CONNECTION_URI || 'mongodb://localhost:27017/superFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
 
 // Logging middleware
 app.use(morgan('common'));
